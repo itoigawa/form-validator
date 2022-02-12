f32bb916a95fa545fa6a2fb073083e915552f16d
itoigawa <707788koo@gmail.com>
2022-02-12 16:43:34 +0900

    add password validate

--- script.js
+++ script.js
@@ -1,27 +1,93 @@
-const form = document.querySelector(".form");
-const formList = document.querySelectorAll(".form .form-control");
+const form = document.querySelector("form");
+const formList = document.querySelectorAll("form .form-control");
 const username = formList[0];
 const usernameInput = username.querySelector("input");
 const usernameValidate = () => {
+  // usernameが3文字以上かつ15文字以下
   if(usernameInput.value.length >= 3 && usernameInput.value.length <= 15) {
-    username.className += " success"
+	username.className += " success";
+    // usernameが3文字未満
   } else if (usernameInput.value.length < 3) {
-    username.className += " error"
+    username.className += " error";
     username.querySelector("small").textContent = "Username must be at least 3 characters";
+    // 上記以外
   } else {
-    username.className += " error"
+    username.className += " error";
     username.querySelector("small").textContent = "Username must be less than 15 characters";
   }
 }
 
 const email = formList[1];
 const emailInput = email.querySelector("input");
+
 const password = formList[2];
 const passwordInput = password.querySelector("input");
 const confirmPassword = formList[3];
 const confirmPasswordInput = confirmPassword.querySelector("input");
 
+const passwordValidate = () => {
+  // パスワードと確認パスワードが完全一致
+  const passwordMatched = passwordInput.value === confirmPasswordInput.value;
+  // パスワードが6文字以上かつ25文字以下
+  const acceptablePassword = passwordInput.value.length >= 6 && passwordInput.value.length <= 25;
+  // パスワードが6文字未満
+  const tooShortPassword = passwordInput.value.length > 0 && passwordInput.value.length < 6;
+  // パスワードが25文字より大きい
+  const tooLongPassword = passwordInput.value.length > 25;
+
+  if(acceptablePassword) {
+    password.className += " success";
+    
+    if(passwordMatched) {
+      confirmPassword.className += " success";
+    } else {
+      confirmPassword.className += " error";
+      confirmPassword.querySelector("small").textContent = "Passwords do not match";
+    }
+	}
+
+  if(tooLongPassword) {
+    password.className += " error";
+    password.querySelector("small").textContent = "Password must be less than 25 characters";
+
+    if(passwordMatched) {
+      confirmPassword.className += " success";
+    } else {
+      confirmPassword.className += " error";
+      confirmPassword.querySelector("small").textContent = "Passwords do not match";
+    }
+  }
+
+  if(tooShortPassword) {
+  	password.className += " error";
+  	password.querySelector("small").textContent = "Password must be at least 6 characters";
+
+    if(passwordMatched) {
+      confirmPassword.className += " success";
+    } else {
+      confirmPassword.className += " error";
+      confirmPassword.querySelector("small").textContent = "Passwords do not match";
+    }
+  }
+
+  // パスワードが空
+  if(!passwordInput.value) {
+  	password.className += " error";
+  	password.querySelector("small").textContent = "Password must be at least 6 characters";
+    // 確認パスワードが空
+    if(!confirmPasswordInput.value) {
+      confirmPassword.className += " error";
+      confirmPassword.querySelector("small").textContent = "Password2 is required";
+    } else {
+      confirmPassword.className += " error";
+      confirmPassword.querySelector("small").textContent = "Passwords do not match";
+    }
+  } 
+
+}
+
 form.addEventListener("submit", (ev) => {
   ev.preventDefault();
   usernameValidate();
+  passwordValidate();
 })
